6d9e4c9648bee4cd242a34a8045a7f58245f2c13
John Soares <john.soares@cwi.com.br>
2026-01-08 10:37:29 -0300

    feat: Adding review entity

---
+++ src/Bookify.Domain/Reviews/Rating.cs
@@ -0,0 +1,20 @@
+﻿using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.Reviews;
+
+public sealed record Rating
+{
+    private Rating(int value) => Value = value;
+
+    public int Value { get; init; }
+
+    public static Result<Rating> Create(int value)
+    {
+        if (value < 1 || value > 5)
+        {
+            return Result.Failure<Rating>(ReviewErrors.Invalid);
+        }
+
+        return new Rating(value);
+    }
+}
