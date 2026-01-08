d11dc7f6ada793603cc596bf5d60e27a57ca2070
John Soares <john.soares@cwi.com.br>
2026-01-08 09:24:05 -0300

    feat: Adding reserve booking command

--- src/Bookify.Domain/Bookings/BookingErrors.cs
+++ src/Bookify.Domain/Bookings/BookingErrors.cs
@@ -6,21 +6,21 @@ public static class BookingErrors
 {
     public static readonly Error NotFound = new(
         "Booking.NotFound",
-        "The booking with the specified identifier was not found");
+        "The booking with the specified identifier was not found.");
 
     public static readonly Error Overlap = new(
         "Booking.Overlap",
-        "The current booking is overlapping with an existing one");
+        "The current booking is overlapping with an existing one.");
 
     public static readonly Error NotReserved = new(
         "Booking.NotReserved",
-        "The booking is not pending");
+        "The booking is not pending.");
 
     public static readonly Error NotConfirmed = new(
         "Booking.NotReserved",
-        "The booking is not confirmed");
+        "The booking is not confirmed.");
 
     public static readonly Error AlreadyStarted = new(
         "Booking.AlreadyStarted",
-        "The booking has already started");
+        "The booking has already started.");
 }
