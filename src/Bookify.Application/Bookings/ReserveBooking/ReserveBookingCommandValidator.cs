eb7420b54e635b1fd4b4675ce77faa2243ce69d4
John Soares <john.soares@cwi.com.br>
2026-01-09 11:23:57 -0300

    feat: Adding validation behavior pipeline.

---
+++ src/Bookify.Application/Bookings/ReserveBooking/ReserveBookingCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace Bookify.Application.Bookings.ReserveBooking;
+
+internal class ReserveBookingCommandValidator : AbstractValidator<ReserveBookingCommand>
+{
+    public ReserveBookingCommandValidator()
+    {
+        RuleFor(x => x.UserId)
+            .NotEmpty().WithMessage("UserId is required.");
+
+        RuleFor(x => x.ApartmentId)
+            .NotEmpty().WithMessage("ApartmentId is required.");
+
+        RuleFor(x => x.StartDate)
+            .LessThan(x => x.EndDate).WithMessage("StartDate must be before EndDate.");
+
+        RuleFor(x => x.EndDate)
+            .GreaterThan(x => x.StartDate).WithMessage("EndDate must be after StartDate.");
+    }
+}
