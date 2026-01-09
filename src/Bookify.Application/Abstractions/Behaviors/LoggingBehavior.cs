849b92576730723443cd0ba5ef94466326b7d5ec
John Soares <john.soares@cwi.com.br>
2026-01-09 10:36:26 -0300

    feat: Adding logging behavior

---
+++ src/Bookify.Application/Abstractions/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using Bookify.Application.Abstractions.Messaging;
+using Microsoft.Extensions.Logging;
+
+namespace Bookify.Application.Abstractions.Behaviors;
+
+public class LoggingBehavior<TRequest, TResponse>
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IBaseCommand
+{
+    private readonly ILogger<TRequest> _logger;
+
+    public LoggingBehavior(ILogger<TRequest> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(
+        TRequest request, 
+        RequestHandlerDelegate<TResponse> next, 
+        CancellationToken cancellationToken)
+    {
+        string requestName = request.GetType().Name;
+
+        try
+        {
+            _logger.LogInformation("Executing command {Command}.", requestName);
+
+            TResponse response = await next();
+
+            _logger.LogInformation("Command {Command} processed successfully.", requestName);
+
+            return response;
+        }
+        catch(Exception exception)
+        {
+            _logger.LogError(exception, "Command {Command} processing failed.", requestName);
+            throw;
+        }
+    }
+}
