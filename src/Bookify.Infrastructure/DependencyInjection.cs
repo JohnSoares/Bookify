c405703cfb87948f3d3415c6b7fcdfe5d1b33d62
John Soares <john.soares@cwi.com.br>
2026-01-09 14:29:47 -0300

    feat: Adding repositories

--- src/Bookify.Infrastructure/DependencyInjection.cs
+++ src/Bookify.Infrastructure/DependencyInjection.cs
@@ -1,7 +1,13 @@
 ﻿using Bookify.Application.Abstractions.Clock;
 using Bookify.Application.Abstractions.Email;
+using Bookify.Domain.Abstractions;
+using Bookify.Domain.Apartments;
+using Bookify.Domain.Bookings;
+using Bookify.Domain.Reviews;
+using Bookify.Domain.Users;
 using Bookify.Infrastructure.Clock;
 using Bookify.Infrastructure.Email;
+using Bookify.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,6 +29,16 @@ public static class DependencyInjection
 
         services.AddDbContext<ApplicationDbContext>(options => 
             options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention());
+
+        services.AddScoped<IUserRepository, UserRepository>();
+
+        services.AddScoped<IApartmentRepository, ApartmentRepository>();
+
+        services.AddScoped<IBookingRepository, BookingRepository>();
+
+        services.AddScoped<IReviewRepository, ReviewRepository>();
+
+        services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<ApplicationDbContext>());
 
         return services;
     }
---
+++ src/Bookify.Infrastructure/Repositories/BookingRepository.cs
@@ -0,0 +1,37 @@
+﻿using Bookify.Domain.Apartments;
+using Bookify.Domain.Bookings;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookify.Infrastructure.Repositories;
+
+internal sealed class BookingRepository : Repository<Booking>, IBookingRepository
+{
+    private static readonly BookingStatus[] ActiveBookingStatuses =
+    {
+        BookingStatus.Reserved,
+        BookingStatus.Confirmed,
+        BookingStatus.Completed
+    };
+
+    public BookingRepository(ApplicationDbContext dbContext)
+        : base(dbContext)
+    {
+    }
+
+    public async Task<bool> IsOverlappingAsync(
+        Apartment apartment, 
+        DateRange duration, 
+        CancellationToken cancellationToken = default)
+    {
+        return await DbContext
+            .Set<Booking>()
+            .AnyAsync(
+                booking =>
+                    booking.ApartmentId == apartment.Id &&
+                    ActiveBookingStatuses.Contains(booking.Status) &&
+                    booking.Duration.Start <= duration.End &&
+                    booking.Duration.End >= duration.Start,
+                cancellationToken);
+    }
+}
+
