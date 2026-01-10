9bd2903e67dfacfbff534a0a0491efb422105c5a
John Soares <john.soares@cwi.com.br>
2026-01-10 17:34:24 -0300

    feat: Create presentation layer(Api) and add services.

---
+++ src/Bookify.Api/Program.cs
@@ -0,0 +1,23 @@
+using Bookify.Application;
+using Bookify.Infrastructure;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
