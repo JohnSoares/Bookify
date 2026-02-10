e935c3be5520b98c0371be91caae6c2ac4f3daf9
John Soares <john.soares@cwi.com.br>
2026-02-10 18:26:18 -0300

    feat: Add permission-based authorization system.

---
+++ src/Bookify.Infrastructure/Authorization/PermissionAuthorizationPolicyProvider.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bookify.Infrastructure.Authorization;
+
+internal sealed class PermissionAuthorizationPolicyProvider : DefaultAuthorizationPolicyProvider
+{
+    private readonly AuthorizationOptions _options;
+
+    public PermissionAuthorizationPolicyProvider(IOptions<AuthorizationOptions> options)
+        : base(options)
+    {
+        _options = options.Value;
+    }
+
+    public override async Task<AuthorizationPolicy?> GetPolicyAsync(string policyName)
+    {
+        AuthorizationPolicy? policy = _options.GetPolicy(policyName);
+
+        if(policy is not null)
+        {
+            return policy;
+        }
+
+        AuthorizationPolicy permissionPolicy = new AuthorizationPolicyBuilder()
+            .AddRequirements(new PermissionRequirement(policyName))
+            .Build();
+
+        _options.AddPolicy(policyName, permissionPolicy);
+
+        return permissionPolicy;
+    }
+}
